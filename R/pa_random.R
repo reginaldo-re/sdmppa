91de25d48a34204db6520376c53dfd1e90787a8a
Reginaldo Ré <reginaldo@utfpr.edu.br>
2024-05-20 12:00:40 -0300

    It correct some lint issues.

--- R/pa_random.R
+++ R/pa_random.R
@@ -1,8 +1,8 @@
 #' Calculate pseudo-absences from an enviromental space P and presences PP
 #'
 #' @description
-#' `pa_random` calculates a set of pseudo-absences points (PAP) **randomly** drawn
-#' from the enviromental space denoted by P.
+#' `pa_random` calculates a set of pseudo-absences points (PAP) **randomly**
+#' drawn from the enviromental space denoted by P.
 #'
 #' @param P A set representing the environmental space.
 #'
@@ -11,31 +11,39 @@
 #' @details
 #'
 #' In fact, the data type of parameter **P** is not a `set`, but a `dataframe`.
-#' Similarly as **P**, **PP** is not really a `set`, It must be a `dataframe` or a `list`.
-#' Points in **PP** represent presence points that wil be removed from **P**, once it is necessary
-#' to reduce **P** so that **PAP** and **PP** keep disjoint. Therefore, **PP** can be:
-#'  1. A `dataframe`, such that at least a name of one column in **P**  must match **PP**.
-#'  2. A named list, such that the name of the list must match a column name in **P**, as an index. So,
-#'  the values in the named list must reference rows in **P** that can be joined using the name of the list.
+#' Similarly as **P**, **PP** is not really a `set`, It must be a `dataframe`
+#' or a `list`. Points in **PP** represent presence points that wil be removed
+#' from **P**, once it is necessary to reduce **P** so that **PAP** and **PP**
+#' keep disjoint. Therefore, **PP** can be:
+#'  1. A `dataframe`, such that at least a name of one column in **P**  must
+#'  match **PP**.
+#'  2. A named list, such that the name of the list must match a column name in
+#'  **P**, as an index. So,
+#'  the values in the named list must reference rows in **P** that can be joined
+#'  using the name of the list.
 #'  3. A list containing both dataframes (as in 1) and named lists (as in 2).
 #'
 #' How is the PPA computed?
 #'
-#' When a `dataframe` is passed to **PP**, all rows in *P* whose column names match *PP* are
-#' compared and removed if they are equal. This reduces the environmental space in *P* to such
-#' that It does not contain *PP*,  and avoids common points in **PP** and **PAP**.
-#' There are two choices when a list is passed: the list is an index referencing rows in *P*,
-#' and the name of the list must match a column in **P**; or, it is an unnamed list containing
-#' data frames and other inner named lists representing several sets **PP**. In both cases, a
-#' `dataframe` or a named list, **PP** should match at least one column in *P*.
+#' When a `dataframe` is passed to **PP**, all rows in *P* whose column names
+#' match *PP* are compared and removed if they are equal. This reduces the
+#' environmental space in *P* to such that It does not contain *PP*, and avoids
+#' common points in **PP** and **PAP**. There are two choices when a list is
+#' passed: the list is an index referencing rows in *P*, and the name of the
+#' list must match a column in **P**; or, it is an unnamed list containing data
+#' frames and other inner named lists representing several sets **PP**. In both
+#' cases, a `dataframe` or a named list, **PP** should match at least one column
+#' in *P*.
 #'
 #' @returns
 #'
-#' The output of the function (PAP)  changes according to the three cases described:
-#'  - If a `dataframe` or a named list is passed as parameter, both representing a single **PP**,
-#'  the value returned is also a dataframe (PAP);
-#'  - If a list representing several different **PP** containing data frames or other named lists
-#'  is passed as a parameter,  the returned value is a list of data frames, one for each **PAP**.
+#' The output of the function (PAP)  changes according to the three cases
+#' described:
+#'  - If a `dataframe` or a named list is passed as parameter, both representing
+#'  a single **PP**, the value returned is also a dataframe (PAP);
+#'  - If a list representing several different **PP** containing data frames or
+#'  other named lists is passed as a parameter,  the returned value is a list of
+#'  data frames, one for each **PAP**.
 #'
 #' @examples
 #' P <- dplyr::tibble(
@@ -86,79 +94,119 @@ pa_random <- function(P, PP) {
 #' @export
 pa_random.data.frame <- function(P, PP = NULL) {
   if (checkmate::test_data_frame(PP)) {
-    checkmate::assert_data_frame(PP, any.missing = F, all.missing = F, null.ok = F, min.rows = 1, max.rows = nrow(P))
-    return (.pa_random(P, PP))
+    return(.pa_random_df(P, PP))
   } else if (checkmate::test_list(PP)) {
-    if (length(PP)==1){
-      if (checkmate::test_data_frame(PP[[1]])){
-        return (pa_random(P, PP[[1]]))
-      } else if (checkmate::test_vector(PP[[1]], strict = T)){
-        checkmate::assert_vector(PP[[1]], any.missing = F, all.missing = F, null.ok = F, max.len = nrow(P), strict = T)
-        id <- names(PP)[1]
-        if (is.null(id) || !(id %in% names(P))) {
-          cli::cli_abort(c("x", "P does not contains PP variable name!"))
-        }
-        PP_joined <- P |>
-          dplyr::filter(!!rlang::sym(id) %in% PP[[id]])
-
-        if(length(PP[[id]])>nrow(PP_joined)){
-          PP_dif_elements = setdiff(PP[[id]], PP_joined[[id]])
-          PP_dif_len = length(PP_dif_elements)
-          cli::cli_warn(c(
-            "The attribute {.var {id}} from PP does not match:",
-            "i" = "There {?is/are} {PP_dif_len} element{?s} left.",
-            "i" = "{id}: {PP_dif_elements}."
-          ))
-        }
-        PAP <- dplyr::lst(
-          !!id := .pa_random(P, PP_joined) |> purrr::pluck(id)
-        )
-        return (PAP)
-      }
+    if (length(PP) == 1) {
+      return(.pa_random_list(P, PP))
     } else {
-
-      possibly_pa_random <- purrr::possibly(.f=pa_random, otherwise = NULL)
-      PP_list <- PP |>
-        purrr::imap(
-          \(x, name_x) {
-            PPA_iter <- NULL
-            name_x <- as.character(name_x)
-            if (name_x==""){
-              if (checkmate::test_data_frame(x)){
-                PPA_iter <- possibly_pa_random(P, x)
-              }
-            } else {
-              if (name_x %in% colnames(P) && (checkmate::test_vector(x, strict=T))){
-                PPA_iter <- possibly_pa_random(P, dplyr::lst(!!name_x:=x)) |>
-                  purrr::pluck(1)
-              }
-            }
-            return(PPA_iter)
-          }
-        )
-      return (PP_list)
+      return(.pa_random_df_list(P, PP))
     }
   }
 
   cli::cli_abort(
     c(
       "x",
-      "PP must be a dataframe (or a list of dataframes), or a list of ids stored in a column of P (or a list os lists)!"
+      "PP must be a dataframe (or a list of dataframes), or a list of ids stored
+      in a column of P (or a list os lists)!"
     )
   )
 }
 
+.pa_random_df <- function(P, PP){
+  checkmate::assert_data_frame(
+    PP,
+    any.missing = FALSE,
+    all.missing = FALSE,
+    null.ok = FALSE,
+    min.rows = 1,
+    max.rows = nrow(P)
+  )
+  return(.pa_random(P, PP))
+}
+
+.pa_random_list <- function(P, PP){
+  if (checkmate::test_vector(PP[[1]], strict = TRUE)) {
+    checkmate::assert_vector(
+      PP[[1]],
+      any.missing = FALSE,
+      all.missing = FALSE,
+      null.ok = FALSE,
+      max.len = nrow(P),
+      strict = TRUE
+    )
+    id <- names(PP)[1]
+    if (is.null(id) || !(id %in% names(P))) {
+      cli::cli_abort(c("x", "P does not contains PP variable name!"))
+    }
+    PP_joined <- P |>
+      dplyr::filter(!!rlang::sym(id) %in% PP[[id]])
+
+    if (length(PP[[id]]) > nrow(PP_joined)) {
+      PP_dif_elements <- setdiff(PP[[id]], PP_joined[[id]])
+      PP_dif_len <- length(PP_dif_elements)
+      cli::cli_warn(
+        c(
+          "The attribute {.var {id}} from PP does not match:",
+          "i" = "There {?is/are} {PP_dif_len} element{?s} left.",
+          "i" = "{id}: {PP_dif_elements}."
+        )
+      )
+    }
+    PAP <- dplyr::lst(!!id := .pa_random(P, PP_joined) |> purrr::pluck(id))
+    return(PAP)
+  }
+}
+
+.pa_random_df_list <- function(P, PP){
+  PP_list <- PP |>
+    purrr::imap(\(x, name_x) {
+      PPA_iter <- NULL
+      name_x <- as.character(name_x)
+      if (checkmate::test_data_frame(x)) {
+        possibly_pa_random <- purrr::possibly(
+            .f = .pa_random_df,
+            otherwise = NULL
+          )
+        PPA_iter <- possibly_pa_random(P, x)
+      } else {
+        if (name_x %in% colnames(P) &&
+            checkmate::test_vector(x, strict = TRUE)){
+          possibly_pa_random <- purrr::possibly(
+              .f = .pa_random_list,
+              otherwise = NULL
+            )
+          PPA_iter <- possibly_pa_random(P, dplyr::lst(!!name_x := x)) |>
+            purrr::pluck(1)
+        }
+      }
+      return(PPA_iter)
+    })
+  return(PP_list)
+}
 
 .pa_random <- function(P, PP = NULL) {
-  checkmate::assert_data_frame(P, any.missing = F, all.missing = F, null.ok = F, min.rows = 1)
-  checkmate::assert_data_frame(PP, any.missing = F, all.missing = F, null.ok = F, min.rows = 1, max.rows = nrow(P))
+  checkmate::assert_data_frame(
+    P,
+    any.missing = FALSE,
+    all.missing = FALSE,
+    null.ok = FALSE,
+    min.rows = 1
+  )
+  checkmate::assert_data_frame(
+    PP,
+    any.missing = FALSE,
+    all.missing = FALSE,
+    null.ok = FALSE,
+    min.rows = 1,
+    max.rows = nrow(P)
+  )
 
 
   names_P <- names(P)
   names_PP <- names(PP)
   names_both <- names_PP[names_PP %in% names_P]
 
-  if (length(names_both)==0){
+  if (length(names_both) == 0) {
     cli::cli_abort(
       c(
         "x",
@@ -167,12 +215,17 @@ pa_random.data.frame <- function(P, PP = NULL) {
     )
   }
 
-
-  P <- P |> dplyr::anti_join(PP, by=names_both)
+  P <- P |>
+    dplyr::anti_join(PP, by = names_both)
 
   set.seed(nrow(PP))
   PAP <- dplyr::sample_n(P, nrow(PP))
 
-  checkmate::assert_data_frame(PAP, any.missing = F, nrows = nrow(PP), null.ok = F)
+  checkmate::assert_data_frame(
+    PAP,
+    any.missing = FALSE,
+    nrows = nrow(PP),
+    null.ok = FALSE
+  )
   return(PAP)
 }
--- tests/testthat/test-pa_random.R
+++ tests/testthat/test-pa_random.R
@@ -1,6 +1,9 @@
-P <- vroom::vroom(test_path("testdata", "predictors.csv"), show_col_types = F)
-PP <- vroom::vroom(test_path("testdata", "argncts_ln.csv"), show_col_types = F)
-PP_predictors <- PP |> dplyr::select(-argncts_ln)
+P <- test_path("testdata", "predictors.csv") |>
+  vroom::vroom(show_col_types = FALSE)
+PP <- test_path("testdata", "argncts_ln.csv") |>
+  vroom::vroom(show_col_types = FALSE)
+PP_predictors <- PP |>
+  dplyr::select(-argncts_ln)
 
 
 test_that(".pa_random - Test invalid P", {
@@ -12,7 +15,8 @@ test_that(".pa_random - Test invalid P", {
   expect_error(
     matrix(nrow = 0, ncol = ncol(P)) |>
       data.frame() |>
-      setNames(P) |> .pa_random(PP_predictors),
+      setNames(P) |>
+      .pa_random(PP_predictors),
     "Assertion on 'P' failed: Must have at least 1 rows, but has 0 rows."
   )
 })
@@ -49,66 +53,14 @@ test_that(".pa_random - Test success case, one less variable", {
   PAP <- P |>
     .pa_random(PP_predictors |> dplyr::select(-c(bio_1)))
 
-  checkmate::expect_data_frame(PAP, any.missing = F, all.missing = F, nrows = nrow(PP_predictors), ncols = ncol(P), null.ok = F)
-  expect_equal(
-    PAP |>
-      dplyr::setdiff(PP_predictors) |>
-      nrow(),
-    nrow(PP)
+  checkmate::expect_data_frame(
+    PAP,
+    any.missing = FALSE,
+    all.missing = FALSE,
+    nrows = nrow(PP_predictors),
+    ncols = ncol(P),
+    null.ok = FALSE
   )
-
-  expect_equal(
-    PAP |>
-      dplyr::setdiff(P) |>
-      nrow(),
-    0
-  )
-})
-
-#--- pa_random.data.frame
-
-test_that("pa_random.data.frame - Test invalid PP", {
-  expect_error(
-    P |>
-      .pa_random(
-        data.frame(matrix(nrow = 0, ncol = ncol(PP_predictors))) |>
-        setNames(PP_predictors)
-      ),
-    "Assertion on 'PP' failed: Must have at least 1 rows, but has 0 rows."
-  )
-
-  expect_error(
-    P |>
-      .pa_random(PP_predictors |> dplyr::bind_rows(P)),
-    "Assertion on 'PP' failed: Must have at most"
-  )
-  expect_error(
-    P |>
-      pa_random(list("cell_id"=c(P$cell_id, c(1)))),
-    "failed: Must have length"
-  )
-  expect_error(
-    P |>
-      pa_random(list("cell_id"=c(PP_predictors$cell_id, c(NA)))),
-    "failed: Contains missing values"
-  )
-  expect_error(
-    P |>
-      pa_random(list("cell_id"=NULL)),
-    "PP must be a dataframe"
-  )
-  expect_error(
-    P |>
-      pa_random(list("cell_id_not_in_P"=PP_predictors$cell_id)),
-    "P does not contains PP variable name!"
-  )
-})
-
-
-test_that("pa_random.data.frame - Test success case, when intersection between PAP and PP (dataframe) is null, |PAP|==|PP|, and PAP in PP", {
-  PAP <- P |>
-    pa_random(PP_predictors)
-
   expect_equal(
     PAP |> dplyr::setdiff(PP_predictors) |> nrow(),
     nrow(PP)
@@ -120,16 +72,62 @@ test_that("pa_random.data.frame - Test success case, when intersection between P
   )
 })
 
+#--- pa_random.data.frame
+
+test_that("pa_random.data.frame - Test invalid PP", {
+  expect_error(
+    P |>
+      .pa_random(
+        data.frame(matrix(nrow = 0, ncol = ncol(PP_predictors))) |>
+          setNames(PP_predictors)
+      ),
+    "Assertion on 'PP' failed: Must have at least 1 rows, but has 0 rows."
+  )
+
+  expect_error(
+    P |> .pa_random(PP_predictors |> dplyr::bind_rows(P)),
+    "Assertion on 'PP' failed: Must have at most"
+  )
+  expect_error(
+    P |> pa_random(list("cell_id" = c(P$cell_id, c(1)))),
+    "failed: Must have length"
+  )
+  expect_error(
+    P |> pa_random(list("cell_id" = c(PP_predictors$cell_id, c(NA)))),
+    "failed: Contains missing values"
+  )
+  expect_null(P |> pa_random(list("cell_id" = NULL)))
+  expect_error(
+    P |> pa_random(list("cell_id_not_in_P" = PP_predictors$cell_id)),
+    "P does not contains PP variable name!"
+  )
+})
+
+
+test_that("pa_random.data.frame - Test success case, when intersection between
+PAP and PP (dataframe) is null, |PAP|==|PP|, and PAP in PP", {
+  PAP <- P |> pa_random(PP_predictors)
+
+  expect_equal(
+    PAP |> dplyr::setdiff(PP_predictors) |> nrow(),
+    nrow(PP)
+  )
+  expect_equal(
+    PAP |> dplyr::setdiff(P) |> nrow(),
+    0
+  )
+})
+
 
 test_that("pa_random.data.frame - Inconsistent PP or list in PP", {
   expect_null(
     P |>
-      pa_random(list("cell_id"=c(PP_predictors$cell_id), "foo"=c(1))) |>
+      pa_random(list("cell_id" = c(PP_predictors$cell_id), "foo" = c(1))) |>
       purrr::pluck(2)
   )
   expect_true(
     P |>
-      pa_random(list("cell_id"=c(PP_predictors$cell_id), "foo"=c(1))) |>
+      pa_random(list("cell_id" = c(PP_predictors$cell_id), "foo" = c(1))) |>
       purrr::pluck(1) |>
       checkmate::test_vector()
   )
@@ -140,15 +138,16 @@ test_that("pa_random.data.frame - Inconsistent PP or list in PP", {
 })
 
 
-test_that("pa_random.data.frame - Test success case, when intersection between PAP and PP (list) is null, |PAP|==|PP|, and PAP in PP", {
+test_that("pa_random.data.frame - Test success case, when intersection between
+PAP and PP (list) is null, |PAP|==|PP|, and PAP in PP", {
+
   expect_warning(
-    P |>
-      pa_random(list("cell_id"=c(PP_predictors$cell_id, c(1)))),
+    P |> pa_random(list("cell_id" = c(PP_predictors$cell_id, c(1)))),
     "from PP does not match"
   )
 
   PAP <- P |>
-    pa_random(list("cell_id"=c(PP_predictors$cell_id)))
+    pa_random(list("cell_id" = c(PP_predictors$cell_id)))
 
   expect_contains(P$cell_id, PAP$cell_id)
   expect_true(!any(PAP$cell_id %in% PP$cell_id))
@@ -160,18 +159,39 @@ test_that("pa_random.data.frame - Test a list of dataframes and named lists", {
       list(
         PP_predictors |> dplyr::select(-c(bio_1)),
         PP_predictors,
-        "cell_id"=PP_predictors$cell_id,
-        list("cell_id"=PP_predictors$cell_id),
-        c(1,2),
-        list(1,2),
-        "cell_id"= PP_predictors
+        "cell_id" = PP_predictors$cell_id,
+        list("cell_id" = PP_predictors$cell_id),
+        c(1, 2),
+        list(1, 2),
+        "cell_id" = PP_predictors
       )
     )
 
-  checkmate::expect_data_frame(PAP[[1]], any.missing = F, all.missing = F, nrows = nrow(PP_predictors), ncols = ncol(P), null.ok = F)
-  checkmate::expect_data_frame(PAP[[2]], any.missing = F, all.missing = F, nrows = nrow(PP_predictors), ncols = ncol(P), null.ok = F)
+  checkmate::expect_data_frame(
+    PAP[[1]],
+    any.missing = FALSE,
+    all.missing = FALSE,
+    nrows = nrow(PP_predictors),
+    ncols = ncol(P),
+    null.ok = FALSE
+  )
+  checkmate::expect_data_frame(
+    PAP[[2]],
+    any.missing = FALSE,
+    all.missing = FALSE,
+    nrows = nrow(PP_predictors),
+    ncols = ncol(P),
+    null.ok = FALSE
+  )
   expect_true(
-    checkmate::test_vector(PAP[[3]], strict = T, any.missing = F, all.missing = F, null.ok = F, len = nrow(PP))
+    checkmate::test_vector(
+      PAP[[3]],
+      strict = TRUE,
+      any.missing = FALSE,
+      all.missing = FALSE,
+      null.ok = FALSE,
+      len = nrow(PP)
+    )
   )
 
   expect_equal(
@@ -201,6 +221,12 @@ test_that("pa_random.data.frame - Test a list of dataframes and named lists", {
 
   expect_null(PAP |> purrr::pluck(5))
   expect_null(PAP |> purrr::pluck(6))
-  expect_null(PAP |> purrr::pluck(7))
+  checkmate::expect_data_frame(
+    PAP[[7]],
+    any.missing = FALSE,
+    all.missing = FALSE,
+    nrows = nrow(PP_predictors),
+    ncols = ncol(P),
+    null.ok = FALSE
+  )
 })
-
